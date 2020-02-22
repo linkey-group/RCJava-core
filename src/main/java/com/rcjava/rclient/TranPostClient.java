a746bced05d7e69a140211e01f75aa72ab347611
brightestboy <yafeng@iscas.ac.cn>
2020-02-23 02:23:01 +0800

    提交签名交易的客户端，包括字符串(HexString)和字节流(fileUpload)两种方式提交交易的方法；

---
+++ src/main/java/com/rcjava/rclient/TranPostClient.java
@@ -0,0 +1,49 @@
+package com.rcjava.rclient;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.rcjava.protos.Peer.Transaction;
+import org.apache.commons.codec.binary.Hex;
+
+/**
+ * 构造交易，提交交易
+ *
+ * @author zyf
+ */
+public class TranPostClient extends Client {
+
+    private String host;
+
+    public TranPostClient(String host) {
+        this.host = host;
+    }
+
+    /**
+     *
+     * @param tran
+     */
+    public JSONObject postTranByString(Transaction tran) {
+        String tranHex = Hex.encodeHexString(tran.toByteArray());
+        String url = "http://" + host + "/transaction/postTranByString";
+        return postTranJString(url, JSON.toJSONString(tranHex));
+    }
+
+    /**
+     *
+     * @param tran
+     */
+    public JSONObject postTranByStream(Transaction tran) {
+        String url = "http://" + host + "/transaction/postTranStream";
+        return postTranStream(url, tran);
+    }
+
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
